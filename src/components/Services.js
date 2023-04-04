8398ad57847418002ea6afcc5478575f488ed6df
Lukas <lukepokorny@protonmail.com>
2023-04-04 11:03:35 +0200

    Initial commit

---
+++ src/components/Services.js
@@ -0,0 +1,53 @@
+import { Title } from "./Title"
+import { services } from "../data"
+
+
+const Services = () => {
+  return (
+    <section className="section services" id="services">
+      
+      <Title title='Our' subTitle='services' />
+      
+      <div className="section-center services-center">
+        {services.map((service) => {
+          const {id, icon, title, text} = service
+          return (
+        <article className="service" kez={id}>
+          <span className="service-icon"><i className={icon}></i></span>
+          <div className="service-info">
+            <h4 className="service-title">{title}</h4>
+            <p className="service-text">
+              {text}
+            </p>
+          </div>
+        </article>
+          )
+
+        })}
+
+        <article className="service">
+          <span className="service-icon"><i className="fas fa-tree fa-fw"></i></span>
+          <div className="service-info">
+            <h4 className="service-title">endless hiking</h4>
+            <p className="service-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Asperiores, officia.
+            </p>
+          </div>
+        </article>
+
+        <article className="service">
+          <span className="service-icon"><i className="fas fa-socks fa-fw"></i></span>
+          <div className="service-info">
+            <h4 className="service-title">amazing comfort</h4>
+            <p className="service-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Asperiores, officia.
+            </p>
+          </div>
+        </article>
+      </div>
+    </section>
+  )
+}
+export default Services
